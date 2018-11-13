07e9aa38ac143328d1d1254899800361d7e69546
Kalvin <ThatGuyKalvin@users.noreply.github.com>
2018-11-13 13:34:51 +0000

    added menu button to PerformanceScreen

--- app/src/main/java/uk/ac/qub/eeecs/game/PerformanceScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/PerformanceScreen.java
@@ -1,8 +1,16 @@
 package uk.ac.qub.eeecs.game;
 
+import android.graphics.Color;
+
+import java.util.List;
+
 import uk.ac.qub.eeecs.gage.Game;
+import uk.ac.qub.eeecs.gage.engine.AssetManager;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.engine.input.Input;
+import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
+import uk.ac.qub.eeecs.gage.ui.PushButton;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
 
 public class PerformanceScreen extends GameScreen {
@@ -14,17 +22,54 @@ public class PerformanceScreen extends GameScreen {
      * @param game Game instance to which the game screen belongs
      */
 
+    private PushButton mReturnToMenuButton;
+    private float mTimeToChange = 0;
+
     public PerformanceScreen(String name, Game game) {
         super(name, game);
+
+        // Load in the bitmaps used on the performance screen
+        AssetManager assetManager = mGame.getAssetManager();
+        assetManager.loadAndAddBitmap("BackArrow", "img/BackArrow.png");
+        assetManager.loadAndAddBitmap("BackArrowSelected", "img/BackArrowSelected.png");
+
+
+        // Define the spacing that will be used to position the buttons
+        int spacingX = (int)mDefaultLayerViewport.getWidth() / 5;
+        int spacingY = (int)mDefaultLayerViewport.getHeight() / 3;
+
+        mReturnToMenuButton = new PushButton(
+                mDefaultLayerViewport.getWidth() * 0.95f,
+                mDefaultLayerViewport.getHeight() * 0.80f,
+                mDefaultLayerViewport.getWidth() * 0.075f,
+                mDefaultLayerViewport.getHeight() * 0.10f,
+                "BackArrow", "BackArrowSelected", this);
+        mReturnToMenuButton.setPlaySounds(true, true);
     }
 
     @Override
     public void update(ElapsedTime elapsedTime) {
+// Process any touch events occurring since the update
+        Input input = mGame.getInput();
 
+        List<TouchEvent> touchEvents = input.getTouchEvents();
+        if (touchEvents.size() > 0 && mTimeToChange > 0.5f) {
+
+            // Update each button and transition if needed
+            mReturnToMenuButton.update(elapsedTime);
+
+            if (mReturnToMenuButton.isPushTriggered())
+                mGame.getScreenManager().removeScreen(this);
+        }
+
+        mTimeToChange += elapsedTime.stepTime;
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
+        // Clear the screen and draw the buttons
+        graphics2D.clear(Color.WHITE);
 
+        mReturnToMenuButton.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
     }
 }
