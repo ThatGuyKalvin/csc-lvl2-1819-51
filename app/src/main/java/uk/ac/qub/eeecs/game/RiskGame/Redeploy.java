933a12ebb8afe39ddf871b0138d96c5bb70427cb
danie <dnelis01@qub.ac.uk>
2018-12-03 12:46:08 +0000

    Updated Redeploy Class

--- app/src/main/java/uk/ac/qub/eeecs/game/RiskGame/Redeploy.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/RiskGame/Redeploy.java
@@ -7,11 +7,31 @@ public class Redeploy {
 
     private float FieldArmyRatio = 0.5f;
     private int minTeams = 3;
-    private Player RPlayer;
-    private ArrayList
+    private Player rPlayer;
+    private ArrayList<Field> rPlayerFields;
 
-     Redeploy (Player)
-    // Check num field
-    // check if area owned
-    // increment field
+    public Redeploy (Player player) {
+
+        rPlayer = player;
+        // rPlayerFields = rPlayer.getListOfFields
+        AssignNewTeams(calculateTeams());
+    }
+
+
+    public int calculateTeams(){
+
+        //calculate length array * ratio
+        // if smaller than min return min
+        // else return big
+
+        // calculate teams added for controlling area(collection of fields)
+
+        return 0;
+    }
+
+    public void AssignNewTeams(int Teams) {
+
+    }
+
+
 }
