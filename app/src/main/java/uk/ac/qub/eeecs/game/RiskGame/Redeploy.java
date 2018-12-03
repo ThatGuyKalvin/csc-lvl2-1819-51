293493c6c30a1ff48c4980925af140f467a202fb
danie <dnelis01@qub.ac.uk>
2018-12-03 12:30:40 +0000

    Update Redeploy

--- app/src/main/java/uk/ac/qub/eeecs/game/RiskGame/Redeploy.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/RiskGame/Redeploy.java
@@ -1,9 +1,16 @@
 package uk.ac.qub.eeecs.game.RiskGame;
 
+import java.util.ArrayList;
+
 public class Redeploy {
 
 
-    // Redeploy (Player)
+    private float FieldArmyRatio = 0.5f;
+    private int minTeams = 3;
+    private Player RPlayer;
+    private ArrayList
+
+     Redeploy (Player)
     // Check num field
     // check if area owned
     // increment field
